a8890e4e41c5db4915993a2615ffccb72c7307ba
Gustavo <guga_3387@hotmail,com>
2023-11-15 18:43:10 -0400

    Curso_ADA_Prog_Concluido

---
+++ meu-projeto-react/src/App.jsx
@@ -0,0 +1,21 @@
+import MeuContador from "./Components/MeuContador";
+// import MinhaLista from "./Components/MinhaLista";
+
+function App() {
+
+  return (
+    <div>
+
+    <MeuContador />
+
+    </div>
+
+
+    // <div>
+    //   <h1>Efeitos Colaterais</h1>
+    //   <MinhaLista></MinhaLista>
+    // </div>
+  )
+}
+
+export default App
---
+++ meu-projeto-react/src/Components/MeuComponente.jsx
@@ -0,0 +1,6 @@
+     export default function MeuComponente(){
+    return (
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi velit laboriosam, incidunt provident sapiente corporis voluptatum sunt at ad mollitia. Cum nisi facilis rerum quam atque adipisci, voluptatem neque!</p>
+    )
+  }
+
